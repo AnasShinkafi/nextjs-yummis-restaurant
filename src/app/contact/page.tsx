35cc3d675e3a65738a967916db30fe043847cb37
Anas <anasisahskf59@gmail.com>
2023-09-11 14:48:11 +0100

    yummis restaurant

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => {
+  return (
+    <div className=' text-red-500'>
+        <h1 className=" text-3xl">08038780968</h1>
+        <h1 className=" text-3xl">yummus@gmail.com</h1>
+    </div>
+  )
+}
+
+export default ContactPage
